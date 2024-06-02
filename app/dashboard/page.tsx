44537a78a3eb6b05941a6efc308c55d969b426e4
Vinu's Mac <Vineet.22566@sscbs.du.ac.in>
2024-06-02 15:12:11 +0530

    Day end commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,4 @@
-import { FetchListsById } from '@/actions/Lists';
-import LeftDash from '@/components/LeftDash'
 import RightDash from '@/components/RightDash'
-import { RedirectToSignIn } from '@clerk/nextjs';
-import { currentUser } from '@clerk/nextjs/server';
 import React from 'react'
 
 function page() {
